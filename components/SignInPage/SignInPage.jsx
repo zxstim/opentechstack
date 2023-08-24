48a046c5f93d9803bcd63f5323c57764022b4d05
0xstim <thang.victor.luong@gmail.com>
2023-08-24 12:31:26 +0700

    new

--- components/SignInPage/SignInPage.jsx
+++ components/SignInPage/SignInPage.jsx
@@ -1,5 +1,5 @@
 import { signIn } from "next-auth/react";
-import Link from "next/link";
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./SignInPage.module.css";
 import { useTranslation } from "next-i18next";
@@ -8,6 +8,10 @@ import NavigationGroup from "../NavigationGroup/NavigationGroup";
 
 export default function SignInPage({ providers, csrfToken }) {
   const { t } = useTranslation("signin");
+  const [passkeyOverlay, setPasskeyOverlay] = useState(false);
+  const [inputEmailValue, setInputEmailValue] = useState('')
+  const [inputUsernameValue, setInputUsernameValue] = useState('')
+
   const paths = {
     fullPath: "/signin",
     pathNamesEn: [
@@ -18,24 +22,33 @@ export default function SignInPage({ providers, csrfToken }) {
     ],
   }
 
+  const handleEmailChange = (event) => {
+    setInputEmailValue(event.target.value); // Update the state with the input's value
+  }
+
+  const handleUsernameChange = (event) => {
+    setInputUsernameValue(event.target.value); // Update the state with the input's value
+  }
+
+  const handleClearForm = () => {
+    setInputEmailValue('');
+    setInputUsernameValue('');
+  }
+
+  function togglePasskeyOverlay() {
+    setPasskeyOverlay(!passkeyOverlay);
+  }
 
   return (
     <div className={styles.signin_layout}>
       <h2>{t("title")}</h2>
-      {/* <div style={{ display: "flex" }}>
-        <Link href="/auth/signin" locale="en" style={{ textDecoration: "none" }}>
-          <p className="i18n-button">🇬🇧</p>
-        </Link>
-        <Link href="/auth/signin" locale="vi" style={{ textDecoration: "none" }}>
-            <p className="i18n-button">🇻🇳</p>
-        </Link>
-      </div> */}
       <LanguageSelector />
       <NavigationGroup paths={paths} />
       {/* <div>
         <button
           className={styles.webauthn_signin_button}
-          onClick={() => signIn("webauthn", { username: "demo" })}
+          // onClick={() => signIn("webauthn", { username: "demo" })}
+          onClick={togglePasskeyOverlay}
         >
           <Image
             src="/icons8-face-id.svg"
@@ -45,6 +58,61 @@ export default function SignInPage({ providers, csrfToken }) {
           />
           <span className={styles.button_text}>{t("method1")}</span>
         </button>
+        {
+          passkeyOverlay ? 
+            <div className={styles.passkey_overlay}>
+              <div className={styles.passkey_form}>
+                <div className={styles.passkey_header}>
+                  <div className={styles.passkey_form_control}>  
+                    <button
+                      className={styles.close_button} 
+                      onClick={togglePasskeyOverlay}
+                    >
+                      Close
+                    </button>
+                    <button
+                      className={styles.clear_button} 
+                      onClick={handleClearForm}
+                    >
+                      Clear
+                    </button>
+                  </div>
+                  <div className={styles.passkey_title}>Enter your email and username</div>
+                </div>
+
+                <div className={styles.passkey_input_container}>
+                  <input 
+                    className={styles.passkey_input} 
+                    type="email" 
+                    placeholder="Email" 
+                    value={inputEmailValue}
+                    onChange={handleEmailChange}
+                    required
+                    />
+                  <input 
+                    className={styles.passkey_input} 
+                    type="text" 
+                    placeholder="Username"
+                    value={inputUsernameValue}
+                    onChange={handleUsernameChange}
+                    required
+                  />
+                </div>
+                <button
+                  className={styles.submit_button}
+                >
+                  <Image
+                    src="/icons8-face-id.svg"
+                    alt="FaceID Logo"
+                    width={25}
+                    height={25}
+                  />
+                  Sign In
+                </button>
+              </div>
+            </div> 
+          : null
+        }
       </div>
       <div className={styles.separator}>
         <span className={styles.separatorText}>{t("separator")}</span>
